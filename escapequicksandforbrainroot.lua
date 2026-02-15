f62f5ee160dc476d0d076fa9e40ded900783dbc7
YazxExploit <yazx31903@gmail.com>
2026-02-15 17:25:01 +0700

    Add script to upgrade Floor1 entities

---
+++ escapequicksandforbrainroot.lua
@@ -0,0 +1,60 @@
+while wait() do
+local args = {
+    "Floor1",
+    1
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Bases"):WaitForChild("UpgradeEntity"):FireServer(unpack(args))
+local args = {
+    "Floor1",
+    2
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Bases"):WaitForChild("UpgradeEntity"):FireServer(unpack(args))
+end
+local args = {
+    "Floor1",
+    3
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Bases"):WaitForChild("UpgradeEntity"):FireServer(unpack(args))
+end
+local args = {
+    "Floor1",
+    4
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Bases"):WaitForChild("UpgradeEntity"):FireServer(unpack(args))
+end
+local args = {
+    "Floor1",
+    5
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Bases"):WaitForChild("UpgradeEntity"):FireServer(unpack(args))
+end
+local args = {
+    "Floor1",
+    6
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Bases"):WaitForChild("UpgradeEntity"):FireServer(unpack(args))
+end
+local args = {
+    "Floor1",
+    7
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Bases"):WaitForChild("UpgradeEntity"):FireServer(unpack(args))
+end
+local args = {
+    "Floor1",
+    8
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Bases"):WaitForChild("UpgradeEntity"):FireServer(unpack(args))
+end
+local args = {
+    "Floor1",
+    9
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Bases"):WaitForChild("UpgradeEntity"):FireServer(unpack(args))
+end
+local args = {
+    "Floor1",
+    10
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Bases"):WaitForChild("UpgradeEntity"):FireServer(unpack(args))
+end
